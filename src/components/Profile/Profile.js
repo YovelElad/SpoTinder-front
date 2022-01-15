e0e0b4ea125a7a7fdc8684a00306073247d4707d
Ofir Du <ofir.duchovne@gmail.com>
2022-01-15 11:17:18 +0200

    top decore is now a component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../../Contexts/UserContext';
-import { Avatar, Container, Box } from '@mui/material';
-import { TextField } from '@mui/material';
-import { Button } from '@mui/material';
+import { Avatar, Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import { width } from '@mui/system';
 import ProfileForm from '../ProfileForm/ProfileForm';
+import TopBackground from '../TopBackground/TopBackground';
 
+//#b14fed
 
 export default function Profile() {
     const {user} = useContext(UserContext);
@@ -14,23 +13,7 @@ export default function Profile() {
     return (
         <div>
         
-        <Box style={{
-            position: "absolute",
-            zIndex: "-1",
-            height: "311px",
-            width: "100%",
-            }}>
-            <svg width="100%" height="311" viewBox="0 0 500 80" preserveAspectRatio="none">
-            <defs>
-                <linearGradient id="myGradient" gradientTransform="rotate(90)">
-                <stop offset="0%"  stopColor="rgba(170, 63, 236, 0.95)" />
-                <stop offset="54%" stopColor="rgba(170, 63, 236, 0.741026)" />
-                <stop offset="100%" stopColor=" rgba(170, 63, 236, 0.6)" />
-                </linearGradient>
-            </defs>
-            <path d="M0,0 L0,70 Q250,80 500,70 L500,0 Z" fill="url('#myGradient')" />
-            </svg>
-        </Box>
+        <TopBackground color="#b14fed"/>
         <Box style={{height: "321px",}}>
             <Box style={{ 
                 justifyContent: "center", 
